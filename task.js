a5e95488ef3a69e8c632a5d1794449e0a699afd3
stodoriko <s.todorikos@gmail.com>
2022-02-15 00:47:33 +0300

    added homework --task1

---
+++ task.js
@@ -0,0 +1,24 @@
+function solveEquation(a, b, c) {
+  let arr = [];
+  let d = b**2-4*a*c;
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d))/(2*a);
+    let x2 = (-b - Math.sqrt(d))/(2*a);
+    arr.push(x1, x2);
+  } else if (d == 0) {
+      let x1 = -b/(2*a);
+      arr.push(x1);
+  }
+  console.log(arr);
+  return arr;
+}
+
+solveEquation(1,2,4);
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+
+  // код для задачи №2 писать здесь
+
+  return totalAmount;
+}
